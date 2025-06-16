0d1993313436a23661826f69ab84721c8bab0278
Huong <huong@brulland.com>
2025-06-16 10:13:55 +0200

    chore:update comment

--- lesson-02/ex3.js
+++ lesson-02/ex3.js
@@ -1,8 +1,8 @@
 const a = 15;
 
-
-if (a > 0) { console.log(a + "là số dương") }
-
+if (a >= 0) {
+    console.log(a + "là số dương")
+}
 if (a < 0) {
     console.log(a + "là số âm")
 
--- lesson-02/ex4.js
+++ lesson-02/ex4.js
@@ -1,3 +1,7 @@
 let height = 160;
+let idealWeight = 60 * 9 / 10;
+let maxWeight = 60;
+let minWeight = 60 * 8 / 10;
 
-console.log("Cân nặng lý tưởng là:" + (60 * 9 / 10) + "," + "Cân nặng tối đa là:" + (60) + "," + "Cân nặng tối thiểu là:" + (60 * 8 / 10));
+
+console.log("Cân nặng lý tưởng là:" + (idealWeight) + "," + "Cân nặng tối đa là:" + (maxWeight) + "," + "Cân nặng tối thiểu là:" + (minWeight));
