496a80c271222288c1062086aa1f6c798a6fc3bd
Huong <huong@brulland.com>
2025-06-16 10:13:55 +0200

    chore: update commit

--- lesson-02/ex5.js
+++ lesson-02/ex5.js
@@ -1,5 +1,3 @@
-
-let i = 100;
 for (let i = 0; i <= 100; i++) {
     if (i % 2 === 1) { console.log(i + "chia cho 2 dư nên là số le") }
     // else { console.log(i + "chia cho 2 ko dư nên là số chan") }
--- lesson-02/ex8.js
+++ lesson-02/ex8.js
@@ -1,4 +1,4 @@
-let i = 20;
-for (i = 1; i <= 100; i += 4) {
+
+for (let i = 1; i <= 100; i += 4) {
     console.log(i);
 }
